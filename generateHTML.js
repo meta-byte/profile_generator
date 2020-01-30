928bd1b2f23c6e66d5dbb6e7354e1092135969c1
Garrett Howard <hylander.garrett@gmail.com>
2020-01-29 20:52:28 -0700

    generateHTML now uses interpolated data and is filled in dynamically

--- generateHTML.js
+++ generateHTML.js
@@ -1,174 +1,148 @@
-const colors = {
-  green: {
-    wrapperBackground: "#E6E1C3",
-    headerBackground: "#C1C72C",
-    headerColor: "black",
-    photoBorderColor: "#black"
-  },
-  blue: {
-    wrapperBackground: "#5F64D3",
-    headerBackground: "#26175A",
-    headerColor: "white",
-    photoBorderColor: "#73448C"
-  },
-  pink: {
-    wrapperBackground: "#879CDF",
-    headerBackground: "#FF8374",
-    headerColor: "white",
-    photoBorderColor: "#FEE24C"
-  },
-  red: {
-    wrapperBackground: "#DE9967",
-    headerBackground: "#870603",
-    headerColor: "white",
-    photoBorderColor: "white"
-  }
-};
 
-function generateHTML(data) {
+
+function generateHTML(git, color, stars) {
+
   return `<!DOCTYPE html>
-<html lang="en">
-   <head>
+  <html lang="en">
+  
+  <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-      <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
       <title>Document</title>
       <style>
-          @page {
-            margin: 0;
+          h1 {
+              color: white;
           }
-         *,
-         *::after,
-         *::before {
-         box-sizing: border-box;
-         }
-         html, body {
-         padding: 0;
-         margin: 0;
-         }
-         html, body, .wrapper {
-         height: 100%;
-         }
-         .wrapper {
-         background-color: ${colors[data.color].wrapperBackground};
-         padding-top: 100px;
-         }
-         body {
-         background-color: white;
-         -webkit-print-color-adjust: exact !important;
-         font-family: 'Cabin', sans-serif;
-         }
-         main {
-         background-color: #E9EDEE;
-         height: auto;
-         padding-top: 30px;
-         }
-         h1, h2, h3, h4, h5, h6 {
-         font-family: 'BioRhyme', serif;
-         margin: 0;
-         }
-         h1 {
-         font-size: 3em;
-         }
-         h2 {
-         font-size: 2.5em;
-         }
-         h3 {
-         font-size: 2em;
-         }
-         h4 {
-         font-size: 1.5em;
-         }
-         h5 {
-         font-size: 1.3em;
-         }
-         h6 {
-         font-size: 1.2em;
-         }
-         .photo-header {
-         position: relative;
-         margin: 0 auto;
-         margin-bottom: -50px;
-         display: flex;
-         justify-content: center;
-         flex-wrap: wrap;
-         background-color: ${colors[data.color].headerBackground};
-         color: ${colors[data.color].headerColor};
-         padding: 10px;
-         width: 95%;
-         border-radius: 6px;
-         }
-         .photo-header img {
-         width: 250px;
-         height: 250px;
-         border-radius: 50%;
-         object-fit: cover;
-         margin-top: -75px;
-         border: 6px solid ${colors[data.color].photoBorderColor};
-         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-         }
-         .photo-header h1, .photo-header h2 {
-         width: 100%;
-         text-align: center;
-         }
-         .photo-header h1 {
-         margin-top: 10px;
-         }
-         .links-nav {
-         width: 100%;
-         text-align: center;
-         padding: 20px 0;
-         font-size: 1.1em;
-         }
-         .nav-link {
-         display: inline-block;
-         margin: 5px 10px;
-         }
-         .workExp-date {
-         font-style: italic;
-         font-size: .7em;
-         text-align: right;
-         margin-top: 10px;
-         }
-         .container {
-         padding: 50px;
-         padding-left: 100px;
-         padding-right: 100px;
-         }
+  
+          .fa {
+              color: black;
+              font-size: 20px;
+          }
+  
+          ul {
+              padding-top: 70px;
+              font-size: 20px;
+          }
+  
+          li {
+              padding-bottom: 30px;
+          }
+  
+          img {
+              margin: 20px;
+              border-style: solid;
+              border-color: white;
+          }
+  
+          blockquote {
+              margin: 20px 0;
+              padding-left: 1.5rem;
+              border-left: 5px solid ${color};
+              padding-right: 1.5rem;
+              border-right: 5px solid ${color};
+          }
+  
+          footer {
+              margin-top: 100px;
+          }
+      </style>
+  </head>
+  
+  <body class="">
+      <div class="">
+          <div class="row ${color} z-depth-1">
+              <div class="col s12 center ${color} darken-2">
+                  <h1>${git.data.name}</h1>
+              </div>
+              <div>
+                  <div class="col s4 center-align">
+                      <ul>
+                          <li><a class="fa fa-location-arrow"></a> ${git.data.location}</li>
+                          <li><a class="fa fa-building"></a> ${git.data.company}</li>
+                      </ul>
+  
+  
+                  </div>
+                  <div class="col s4">
+                      <img class="responsive-img circle z-depth-2"
+                          src="${git.data.avatar_url}">
+                  </div>
+                  <div class="col s4 center-align">
+                      <ul>
+                          <li><a href="${git.data.html_url}" target="_blank" class="fa fa-github"></a> Github</li>
+                          <li><a href="${git.data.blog}" target="_blank" class="fa fa-rss"></a> Blog</li>
+  
+                      </ul>
+                  </div>
+              </div>
+  
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col s12 center">
+                      <blockquote class="">
+                          <h5>${git.data.bio}</h5>
+                      </blockquote>
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col s6 center">
+                      <div class="card ${color}">
+                          <div class="card-content white-text">
+                              <span class="card-title">Public Repositories</span>
+                              <h3>${git.data.public_repos}</h3>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col s6 center">
+                      <div class="card ${color}">
+                          <div class="card-content white-text">
+                              <span class="card-title">Starred</span>
+                              <h3>${stars}</h3>
+                          </div>
+                      </div>
+                  </div>
+  
+              </div>
+              <div class="row">
+                  <div class="col s6 center">
+                      <div class="card ${color}">
+                          <div class="card-content white-text">
+                              <span class="card-title">Followers</span>
+                              <h3>${git.data.followers}</h3>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col s6 center">
+                      <div class="card ${color}">
+                          <div class="card-content white-text">
+                              <span class="card-title">Following</span>
+                              <h3>${git.data.following}</h3>
+                          </div>
+                      </div>
+                  </div>
+  
+              </div>
+  
+          </div>
+      </div>
+  
+      <footer class="page-footer ${color}">
+          <div class="footer-copyright ${color} darken-2">
+              <div class="container">
+              </div>
+          </div>
+      </footer>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  
+  </body>
+  
+  </html>`
+}
 
-         .row {
-           display: flex;
-           flex-wrap: wrap;
-           justify-content: space-between;
-           margin-top: 20px;
-           margin-bottom: 20px;
-         }
-
-         .card {
-           padding: 20px;
-           border-radius: 6px;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           margin: 20px;
-         }
-         
-         .col {
-         flex: 1;
-         text-align: center;
-         }
-
-         a, a:hover {
-         text-decoration: none;
-         color: inherit;
-         font-weight: bold;
-         }
-
-         @media print { 
-          body { 
-            zoom: .75; 
-          } 
-         }
-      </style>`
-        }
+module.exports = generateHTML
